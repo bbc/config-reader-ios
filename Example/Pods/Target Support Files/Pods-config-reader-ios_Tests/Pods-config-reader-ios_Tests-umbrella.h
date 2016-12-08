8f3d1e6f723349fe341480d5e2d4cf6e3f53f330
ramdhany <nirish777@gmail.com>
2016-12-08 19:53:04 +0000

    removed source from branch

--- Example/Pods/Target Support Files/Pods-config-reader-ios_Tests/Pods-config-reader-ios_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_config_reader_ios_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_config_reader_ios_TestsVersionString[];
-
--- Example/Pods/Target Support Files/config-reader-ios/config-reader-ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_config_reader_ios : NSObject
-@end
-@implementation PodsDummy_config_reader_ios
-@end
